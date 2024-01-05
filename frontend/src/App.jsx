36c8841c62fd9a873f3ad72fc63f1cec62b04c2b
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-01-05 17:20:45 +0530

    shopcontext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,10 @@ import ShopCategory from './Pages/ShopCategory'
 import Product from './Pages/Product'
 import Cart from './Pages/Cart'
 import LoginSignup from './Pages/LoginSignup'
+import men_banner from "./Components/assets/banner_mens.png"
+import women_banner from "./Components/assets/banner_women.png"
+import kids_banner from "./Components/assets/banner_kids.png"
+
 function App() {
   return (
     <>
@@ -15,9 +19,9 @@ function App() {
       <div className="body-wrapper">
         <Routes>
         <Route path="/" element={<Shop />} />
-        <Route path="/men" element={<ShopCategory category="men" />} />
-        <Route path="/women" element={<ShopCategory category="women" />} />
-        <Route path="/kids" element={<ShopCategory category="kids" />} />
+        <Route path="/men" element={<ShopCategory category="men" banner={men_banner} />} />
+        <Route path="/women" element={<ShopCategory category="women" banner={women_banner} />} />
+        <Route path="/kids" element={<ShopCategory category="kid" banner={kids_banner} />} />
         <Route path="/product" element={<Product />}>
           <Route path=':productId' element={<Product />} />
         </Route>
@@ -25,7 +29,7 @@ function App() {
         <Route path='/login' element={<LoginSignup />} />
       </Routes>
       </div>
-      <Footer />
+      {/* <Footer /> */}
       
     </BrowserRouter>
     </>
--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import "./pagesCss/shopCategory.css"
+import { ShopContext } from '../Context/ShopContext'
+import dropdown_icon from "../Components/assets/dropdown_icon.png"
+import Item from '../Components/Item/Item'
+function ShopCategory({category, banner}) {
 
-function ShopCategory({category}) {
+  const {all_product} = useContext(ShopContext)
+
+
+
   return (
-    <div>ShopCategory: {category}</div>
+    <>
+    <div className="shop-category">
+      <img src={banner} alt= {`${category}_banner`} className='banner'/>
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="dropdown_icon" />
+        </div>
+      </div>
+      <div className="shopcategory-products">
+        {
+          all_product.map((product, i)=>{
+            if(category === product.category){
+              return (<Item key={i} image={product.image} name={product.name} new_price={product.new_price} old_price={product.old_price} />)
+            }else{
+              return null 
+            }
+          })
+        }
+      </div>
+    </div>
+    </>
   )
 }
 
