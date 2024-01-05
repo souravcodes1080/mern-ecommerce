110ace8a70b77e51ec3ee09c8eeec2bae6472d8f
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-01-05 17:36:32 +0530

    navbar fixed

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="nav-logo">
-        <img src={logo} alt="brand_logo" />
+        <img src={logo} alt="brand_logo" width={"50px"} />
         <p>SHOPSY</p>
       </div>
       <ul className="nav-menu">
@@ -20,37 +20,59 @@ function Navbar() {
             setMenu("shop");
           }}
         >
-          <Link to="/" style={{textDecoration: "none", color: "black"}} >Shop {menu === "shop" ? <hr /> : <></>}</Link>
+          <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+            Shop {menu === "shop" ? <hr /> : <></>}
+          </Link>
         </li>
         <li
           onClick={() => {
             setMenu("men");
           }}
         >
-          <Link to="/men" style={{textDecoration: "none", color: "black"}} >Men {menu === "men" ? <hr /> : <></>}</Link>
+          <Link to="/men" style={{ textDecoration: "none", color: "black" }}>
+            Men {menu === "men" ? <hr /> : <></>}
+          </Link>
         </li>
         <li
           onClick={() => {
             setMenu("women");
           }}
         >
-          <Link to="/women" style={{textDecoration: "none", color: "black"}} >Women {menu === "women" ? <hr /> : <></>}</Link>
+          <Link to="/women" style={{ textDecoration: "none", color: "black" }}>
+            Women {menu === "women" ? <hr /> : <></>}
+          </Link>
         </li>
         <li
           onClick={() => {
             setMenu("kids");
           }}
         >
-          <Link to="/kids" style={{textDecoration: "none", color: "black"}} >Kids {menu === "kids" ? <hr /> : <></>}</Link>
+          <Link to="/kids" style={{ textDecoration: "none", color: "black" }}>
+            Kids {menu === "kids" ? <hr /> : <></>}
+          </Link>
         </li>
       </ul>
       <div className="nav-login-cart">
         <Link to="/login">
-          <button className="nav-login">Login</button>
+          <button
+            onClick={() => {
+              setMenu("login");
+            }}
+            className="nav-login"
+          >
+            Login
+          </button>
         </Link>
 
         <Link to="/cart">
-          <img src={cart_icon} alt="cart_icon" />{" "}
+          <img
+            onClick={() => {
+              setMenu("cart");
+            }}
+            src={cart_icon}
+            alt="cart_icon"
+            width={"70%"}
+          />
         </Link>
         <div className="nav-cart-count">0</div>
       </div>
