c071960fc1b685e534ae55593850f5243b139b51
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-01-05 18:41:54 +0530

    login page

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import React from "react";
+import "./pagesCss/loginSignup.css";
 function LoginSignup() {
   return (
-    <div>LoginSignup</div>
-  )
+    <>
+      <div className="loginSignup">
+        <div className="loginSignup-container">
+          <h1>Sign Up</h1>
+          <div className="loginSignup-fields">
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+          </div>
+
+          <p className="loginSignup-login">
+            Already have an account? <span>Login</span>
+          </p>
+          <div className="loginSignup-agree">
+            <input type="checkbox" name="" id="" />
+            <p>By continuing, I agree to the terms of use & policy privacy.</p>
+          </div>
+          <button>Continue</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default LoginSignup
+export default LoginSignup;
