88f07a248f9899b08b1b5b8845484bf7e57fb7a6
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-01-05 17:43:39 +0530

    all changes saved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         <Route path='/login' element={<LoginSignup />} />
       </Routes>
       </div>
-      {/* <Footer /> */}
+      <Footer />
       
     </BrowserRouter>
     </>
