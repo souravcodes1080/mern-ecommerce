6c22317adbac09d2ba06da3e6ebd16d642c50103
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-01-16 13:54:33 +0530

    admin panel frontend setup

---
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./navbar.css"
+function Navbar() {
+  return (
+    <>
+    <div className="navbar">
+        
+    </div>
+    </>
+  )
+}
+
+export default Navbar
---
+++ admin/src/Pages/Admin/Admin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./admin.css"
+function Admin() {
+  return (
+    <>
+        <div className="admin">
+
+        </div>
+    </>
+  )
+}
+
+export default Admin
