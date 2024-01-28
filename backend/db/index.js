8d2ba3494521bca04166c9697c68106c74869e6f
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-01-28 21:15:20 +0530

    vercel config

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDb = async ()=>{
     try{
-        const db = await mongoose.connect(process.env.MONGODB_URI)
+        const db = await mongoose.connect("mongodb+srv://techburnt06:sourav123@cluster0.vswscpy.mongodb.net/shopsy?retryWrites=true&w=majority")
         console.log(`Connnected to DB: ${db.connection.name} on port: ${db.connection.port}`)
     }catch(err){
         console.error(err)
